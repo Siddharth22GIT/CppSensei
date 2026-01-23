8fda196c2ad2516ff5f3ff55d28a66d6f3e1dc8c
Siddharth Mishra <siddharthmishra10e@gmail.com>
2026-01-23 22:33:19 +0530

    Change default port from 5000 to 7860
    Signed-off-by: Siddharth Mishra <siddharthmishra10e@gmail.com>

--- app.py
+++ app.py
@@ -118,6 +118,6 @@ Feedback:
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
 
